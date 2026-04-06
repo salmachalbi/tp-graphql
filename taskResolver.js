b4799061ca99cc85dda37beee6dd81da6183c71c
salmachalbi <salma.chalbi@polytechnicien.tn>
2026-04-06 04:46:19 +0100

    Ajout du champ duration (Int) dans le schéma et les résolveurs

--- taskResolver.js
+++ taskResolver.js
@@ -5,18 +5,24 @@ id: '1',
 title: 'Développement Front-end pour Site E-commerce',
 description: 'Créer une interface utilisateur réactive en utilisant React et Redux pour un site e-commerce.',
 completed: false,
+duration: 10,
+
 },
 {
 id: '2',
 title: 'Développement Back-end pour Authentification Utilisateur',
 description: "Implémenter un système d'authentification et d'autorisation pour une application web en utilisant Node.js, Express, et Passport.js",
 completed: false,
+duration: 15,
+
 },
 {
 id: '3',
 title: 'Tests et Assurance Qualité pour Application Web',
 description: 'Développer et exécuter des plans de test et des cas de test complets.',
 completed: false,
+duration: 8,
+
 },
 ];
 const taskResolver = {
@@ -25,12 +31,13 @@ task: (_, { id }) => tasks.find(task => task.id === id),
 tasks: () => tasks,
 },
 Mutation: {
-addTask: (_, { title, description, completed }) => {
+addTask: (_, { title, description, completed, duration }) => {
 const task = {
 id: String(tasks.length + 1),
 title,
 description,
 completed,
+duration,
 };
 tasks.push(task);
 return task;
